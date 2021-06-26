1fb6c4dfa04af5542ee70d5e8318ee9a7f791e3e
akkshaths <63518142+akkshaths@users.noreply.github.com>
2021-06-26 16:10:12 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -12,13 +12,18 @@ class editSchedule:
 
     def addActivity(self, add, timing):
         #add is the names of the club as a list
-        #timing is the times of the activities in a list
-        
+        #timing is the times of the activities in a list in form ['8:30', '6:45']
+
+        d = []
+        for x in timing:
+            d.append(datetime.strptime(x, '%H:%M').time())
+
+
         c = []
         for x in list(filter(None, list(self.df['Time']))):
             c.append(datetime.strptime(x, '%H:%M:%S').time())
         
-        list1 = timing + c
+        list1 = d + c
         listTime = sorted(list1)
         #return sorted(list1)
         list2 = add + list(self.df['Class/Club'])
@@ -38,10 +43,8 @@ class editSchedule:
 
 
 
+
+
 y = editSchedule()
-timing = ['10:30', '8:45', '6:30']
-c = []
-for x in timing:
-    c.append(datetime.strptime(x, '%H:%M').time())
-y.addActivity(['Soccer','Economics','Finance'], c)
+y.addActivity(['Soccer','Economics','Finance'], ['10:30', '8:45', '6:30'])
 
