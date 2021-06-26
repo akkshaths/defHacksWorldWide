a3bbf974ec4961e39fb4ea25a9ca5f87a2fa5525
akkshaths <63518142+akkshaths@users.noreply.github.com>
2021-06-26 08:09:25 -0400

    Update clubInterest.py

--- clubInterest.py
+++ clubInterest.py
@@ -1,40 +1,65 @@
 import pandas as pd
 import openpyxl
 
-data = pd.read_excel('clubList.xlsx', nrows = 29)
 
-df = pd.DataFrame(data, columns= ['Clubs','Address', 'Tag1','Tag2','Tag3', 'Social Media Links', 'Website Link'])
-
-club = df['Clubs']
-tag1 = df['Tag1']
-tag2 = df['Tag2']
-tag3 = df['Tag3']
-address = df['Address']
-social = df['Social Media Links']
-link = df['Website Link']
 
 class Filter:
-    def __init__(self, interest):
-        self.interest = interest
+    def __init__(self):
+        self.data = pd.read_excel('clubList.xlsx', nrows = 29)
 
-    def returnClubNames(self):
+        self.df = pd.DataFrame(self.data, columns= ['Clubs','Address', 'Tag1','Tag2','Tag3', 'Social Media Links', 'Website Link'])
+
+        self.club = self.df['Clubs']
+        self.tag1 = self.df['Tag1']
+        self.tag2 = self.df['Tag2']
+        self.tag3 = self.df['Tag3']
+        self.address = self.df['Address']
+        self.social = self.df['Social Media Links']
+        self.link = self.df['Website Link']
+        
+
+    def returnClubNames(self, interest):
         dict1 = {
             'Club Name': [],
-            'Social Media': [],
             'Address': [],
+            'Social Media': [],
             'Link': []
         }
-        for x in range(0, len(club)):
-            if tag1[x] in self.interest or tag2[x] in self.interest or tag3[x] in self.interest:
-                dict1['Club Name'].append(club[x])
-                dict1['Address'].append(address[x])
-                dict1['Link'].append(link[x])
-                dict1['Social Media'].append(social[x])
+        for x in range(0, len(self.club)):
+            if self.tag1[x] in interest or self.tag2[x] in interest or self.tag3[x] in interest:
+                dict1['Club Name'].append(self.club[x])
+                dict1['Address'].append(self.address[x])
+                dict1['Link'].append(self.link[x])
+                dict1['Social Media'].append(self.social[x])
 
         dataFrame = pd.DataFrame.from_dict(dict1)
+        return dataFrame.to_string(index=False)
+
+    def searchFor(self, query):
+        dict2 = {
+            'Club Name': [],
+            'Address': [],
+            'Social Media': [],
+            'Link': []
+        }
+        if query in list(self.club):
+            x = list(self.club).index(query)
+            dict2['Club Name'].append(self.club[x])
+            dict2['Address'].append(self.address[x])
+            dict2['Link'].append(self.link[x])
+            dict2['Social Media'].append(self.social[x])
+        dataFrame = pd.DataFrame.from_dict(dict2)
+        return dataFrame.to_string(index=False)
         return dataFrame
 
+    def printting(self):
+        return(self.club)
 
 
-y = Filter(['Finance', 'Engineering', 'Medical'])
-print(y.returnClubNames())
+
+
+y = Filter()
+print(y.searchFor('Economics Club'))
+# ansh = list(y.printting())
+# if 'Economics Club' in ansh:
+#     print('success')
